ee56ebe873e64151f2de083681e45e362b738497
Mustafayev Tural Elbrus  <Must_hj90@itstep.edu.az>
2019-06-13 19:43:44 +0400

    Add project files.

---
+++ WebApplication1/Controllers/ApiController.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication1.Controllers
+{
+    public class TestController : ApiController
+    {
+        List<TestEntity> list;
+
+        public TestController()
+        {
+            list = new List<TestEntity>();
+            list.Add(new TestEntity() { Id = 1, Name = "Test1" });
+        }
+
+        public string Get(string json)
+        {
+            TestModel model = JsonConvert.DeserializeObject<TestModel>(json);
+            TestEntity ent =  list.FirstOrDefault(x=> x.Id == model.Id);
+            return JsonConvert.SerializeObject(ent);
+
+        }
+
+    }
+}
---
+++ WebApplication1/TestEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1
+{
+    public class TestEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
